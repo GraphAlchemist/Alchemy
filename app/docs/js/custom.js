9857e8ea3b258105c34272e1c42d466fa389fdf6
huston <h@graphalchemist.com>
2014-06-27 10:45:28 -0700

    cleaning up docs

--- app/docs/js/custom.js
+++ app/docs/js/custom.js
@@ -1,4 +1,5 @@
 // hacky approach to help make the sidebar scrollable
 $( document ).ready(function() {
-    $('ul.current').css('max-height', $(window).height());
+    $('ul.current').css('max-height', $(window).height()/1.2);
+    // add window resize function
 });
