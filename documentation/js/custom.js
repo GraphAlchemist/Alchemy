7bf2e22fbb5f5869ea0656cd404ab7fffdf7b0f3
huston <h@graphalchemist.com>
2014-07-02 18:50:24 -0700

    Updates

---
+++ documentation/js/custom.js
@@ -0,0 +1,5 @@
+// hacky approach to help make the sidebar scrollable
+$( document ).ready(function() {
+    $('ul.current').css('max-height', $(window).height()/1.4);
+    // add window resize function
+});
